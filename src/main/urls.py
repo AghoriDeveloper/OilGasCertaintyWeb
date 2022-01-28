e4d005729a69d50ed4eae0f8d97ac63784462432
AghoriDeveloper <nagarkaronkark@gmail.com>
2022-01-28 22:01:47 +0530

    heroku changes

--- src/main/urls.py
+++ src/main/urls.py
@@ -18,11 +18,18 @@ from django.urls import path, include
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
+from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
+
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('ObjectA.urls'), name="obj-a"),
     path('obj-a/', include('ObjectA.urls')),
     path('obj-b/', include('ObjectB.urls')),
     path('obj-abc/', include('ObjectABC.urls')),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('image/favicon.ico')))
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('image/favicon.ico'))),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
